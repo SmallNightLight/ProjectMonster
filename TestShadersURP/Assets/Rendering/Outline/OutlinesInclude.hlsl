cde6879c65c86c967f3dc81651f589e7a8df6edf
SmallNightLight <matthiaskruis@gmail.com>
2024-05-28 12:04:57 +0200

    Refined Outllines
    Added depth based outlines

--- TestShadersURP/Assets/Rendering/Outline/OutlinesInclude.hlsl
+++ TestShadersURP/Assets/Rendering/Outline/OutlinesInclude.hlsl
@@ -3,14 +3,13 @@
 #ifndef OUTLINES_INCLUDED
 #define OUTLINES_INCLUDED
 
-void GetCrossSampleUVs_float(float4 UV, float2 texelSize, float4 offsetMultiplier,
-	out float2 UVOriginal, out float2 UVTopRight, out float2 UVBottomLeft, out float2 UVTopLeft, out float2 UVBottomRight)
+void GetCrossSampleUVs_float(float4 UV, float2 texelSize, float4 multiplier,
+	out float2 UVTopRight, out float2 UVBottomLeft, out float2 UVTopLeft, out float2 UVBottomRight)
 {
-	UVOriginal = UV;
-	UVTopRight = UV.xy + float2(texelSize.x, texelSize.y) * offsetMultiplier[0];
-	UVBottomLeft = UV.xy - float2(texelSize.x, texelSize.y) * offsetMultiplier[1];
-	UVTopLeft = UV.xy + float2(-texelSize.x * offsetMultiplier[2], texelSize.y * offsetMultiplier[2]);
-	UVBottomRight =  UV.xy + float2(texelSize.x * offsetMultiplier[3], -texelSize.y * offsetMultiplier[3]);
+	UVTopRight = UV.xy + float2(texelSize.x, texelSize.y) * multiplier[0];
+	UVBottomLeft = UV.xy - float2(texelSize.x, texelSize.y) * multiplier[1];
+	UVTopLeft = UV.xy + float2(-texelSize.x * multiplier[2], texelSize.y * multiplier[2]);
+	UVBottomRight =  UV.xy + float2(texelSize.x * multiplier[3], -texelSize.y * multiplier[3]);
 }
 
 #endif
