ea8312eb80cdd8e9529d09f7ff299aff9ff4931b
SmallNightLight <matthiaskruis@gmail.com>
2024-06-06 16:48:09 +0200

    Explosion effect
    Added a shader for the explosion effect and made it instantiate on ground impact. Added the code for the kart the register a hit

--- UnityMonsterProject/Assets/Scripts/Abilities/AbilityFireball.cs
+++ UnityMonsterProject/Assets/Scripts/Abilities/AbilityFireball.cs
@@ -8,6 +8,13 @@ public class AbilityFireball : MonoBehaviour
     [SerializeField] private float _power;
     [SerializeField] private float _gravity;
 
+    [SerializeField] private string _hitTag;
+
+    [SerializeField] private GameObject _impactPrefab;
+    [SerializeField] private float _checkoffset = 1;
+
+    private bool _isExploded;
+
     private void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
@@ -18,5 +25,24 @@ public class AbilityFireball : MonoBehaviour
     {
         _rigidbody.AddForce(new Vector3(0, _gravity , 0) * _rigidbody.mass);
     }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == _hitTag && !_isExploded)
+        {
+            Vector3 rayStartPoint = transform.position + new Vector3(0, _checkoffset, 0);
 
+            RaycastHit hit;
+            if (Physics.Raycast(rayStartPoint, Vector3.down, out hit))
+            {
+                Explode(hit.point);
+            }
+        }
+    }
+
+    private void Explode(Vector3 impactPosition)
+    {
+        _isExploded = true;
+        GameObject impactEffect = Instantiate(_impactPrefab, impactPosition, Quaternion.identity);
+        Destroy(impactEffect, 2);
+    }
 }
