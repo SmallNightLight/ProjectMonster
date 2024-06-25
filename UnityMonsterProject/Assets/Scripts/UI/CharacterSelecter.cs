3dedf65b101f6fd7a739dccb7ba408225b62cdfa
SmallNightLight <matthiaskruis@gmail.com>
2024-06-26 00:16:12 +0200

    character select update
    Added improved visuals and select handle

--- UnityMonsterProject/Assets/Scripts/UI/CharacterSelecter.cs
+++ UnityMonsterProject/Assets/Scripts/UI/CharacterSelecter.cs
@@ -1,12 +1,43 @@
 using ScriptableArchitecture.Data;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
 
 public class CharacterSelecter : MonoBehaviour
 {
+    [SerializeField] private StringReference _sceneName;
+
     [SerializeField] private GameDataReference _gameData;
+    [SerializeField] private UnityEvent _onCharactersSelected;
+
+    private int _player1Selected, _player2Selected;
+    private bool _loading;
 
     private void Start()
     {
-        
+        _player1Selected = _player2Selected = 0;
+    }
+
+    public void PlayerSelected(int player)
+    {
+        if (player == 1)
+            _player1Selected = 1;
+        else if (player == 2)
+            _player2Selected = 1;
+
+        if (_player1Selected + _player2Selected >= _gameData.Value.PlayerCount)
+        {
+            _onCharactersSelected.Invoke();
+            LoadScene();
+        }
+    }
+
+    public void LoadScene()
+    {
+        if (!_loading)
+        {
+            _loading = true;
+            SceneManager.LoadSceneAsync(_sceneName.Value);
+        }
     }
 }
--- UnityMonsterProject/Assets/Scripts/UI/SetCharacterData.cs
+++ UnityMonsterProject/Assets/Scripts/UI/SetCharacterData.cs
@@ -1,10 +1,24 @@
 using ScriptableArchitecture.Data;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
 
-public class SetCharacterData : MonoBehaviour
+public class SetCharacterData : MonoBehaviour, ISelectHandler, IPointerEnterHandler
 {
     [SerializeField] private GameDataReference _gameData;
     [SerializeField] private int _playerIndex;
+
+    [SerializeField] private UnityEvent _onSelect;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        _onSelect.Invoke();
+    }
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        _onSelect.Invoke();
+    }
 
     public void Set(CharacterDataVariable characterData)
     {
